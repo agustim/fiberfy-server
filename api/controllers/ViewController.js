c51e5a86bc667581642f8f32b441e9014c51a293
agusti <agusti.moll@guifi.net>
2016-07-04 06:43:06 +0200

    [WiP]: Add login screen

--- api/controllers/ViewController.js
+++ api/controllers/ViewController.js
@@ -7,4 +7,8 @@ module.exports = class ViewController extends Controller {
   index (request, reply) {
     reply.render('index',{})
   }
+
+  map (request, reply) {
+    reply.render('map', {})
+  }
 }
