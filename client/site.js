0903776c1818af6cc2be6aa209eb7f033789929d
agusti <agusti.moll@guifi.net>
2016-07-08 18:13:08 +0200

    Fixed Active site in build path

--- client/site.js
+++ client/site.js
@@ -117,7 +117,7 @@ Site.prototype.onSiteClick = function (e){
       } else {
         // No n'hi ha cap actiu, el creem.
         console.log('inici tram.');
-        this.changeTypeIcon('acitve');
+        this.changeTypeIcon('active');
         this.map_parent.active_path = new Path(null, null, null, null, new Array(), this.map_parent.type_path_default, this.map_parent);
         this.map_parent.active_path.setFirstSite(this);
       }
