028b7acf81ecf8e1099c11480db2a4411f0c3da7
agusti <agusti.moll@guifi.net>
2016-09-28 15:29:46 +0200

    project modify

--- client/projecte.js
+++ client/projecte.js
@@ -6,6 +6,9 @@ var Projecte = function(id, name, m){
   this.id = id;
   this.name = name;
   this.status = "define";
+  this.latitude = null;
+  this.longitude = null;
+  this.zoom = null;
   this.map_parent = m;
 };
 
@@ -13,14 +16,19 @@ Projecte.prototype.save = function (){
   var that = this;
   strUrl = this.map_parent.serverUrl + "/project";
   console.log('API call: ' + strUrl);
+  var data = {}
+  var prop = [ "name", "status", "latitude", "longitude", "zoom" ];
+  prop.forEach(function(item) {
+      if (this[item]) data[item] = this[item];
+  });
   if (this.id == 0 || this.id == null) {
-    $.post( strUrl, JSON.stringify({ "name": this.name, "status": this.status }))
+    $.post( strUrl, JSON.stringify(data))
       .done(function( data ) {
         that.map_parent.notify("Updated!");
         that.id = data.id;
       }, "json");
   } else {
-    $.put( strUrl+"/"+this.id, JSON.stringify({ "name": this.name, "status": this.status }))
+    $.put( strUrl+"/"+this.id, JSON.stringify(data))
       .done(function( data ) {
         that.map_parent.notify("Updated!");
       }, "json");
